92425495057c0e3ba445ea434e13f82dce60e206
william_ST <sulca.03.1995@gmail.com>
2017-01-30 13:30:56 -0500

    Initial Commit

---
+++ Homework02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework02
+//
+//  Created by William Sulca Talavera on 30/01/17.
+//  Copyright © 2017 William Sulca Talavera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
